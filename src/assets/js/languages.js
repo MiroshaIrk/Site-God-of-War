99e76d15c484fe132936d16d0ead22f9ceb2d15b
MiroshaIrk <106314482+MiroshaIrk@users.noreply.github.com>
2022-12-09 18:49:08 +0800

    Add files via upload

---
+++ src/assets/js/languages.js
@@ -0,0 +1,18 @@
+export const languages = {
+    en: {
+        editions: 'Editions',
+        controller: 'Controller',
+        about: 'About game',
+        explore: 'Explore',
+        faq: 'FAQ',
+        news: 'News',
+    },
+    ru: {
+        editions: 'издание',
+        controller: 'манипулятор',
+        about: 'об игре',
+        explore: 'иследовать',
+        faq: 'ЧаВо',
+        news: 'новости',
+    }
+};
